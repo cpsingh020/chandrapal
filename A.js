af5ef080fa2db961e2178e1fda39a048c78ad501
cpsingh020 <sisodiyacpsingh020@gmail.com>
2025-06-24 09:49:03 +0530

    first commit

---
+++ A.js
@@ -0,0 +1,53 @@
+//  1 =example
+/*var a;
+console.log(a);        
+a=10
+console.log(a);*/
+
+
+// example=2
+// let classDetails = {
+//     name:"cp",               
+//     role:"student",
+//     id:3
+
+// }
+// console.log(classDetails['name']);
+
+// example=3;
+// function hello(){
+//     let a=18;
+//     words=2;
+//     id:2
+   
+// }
+//  console.log( hello['a']);
+
+// example=4;
+//  let fruits="apple";
+//  console.log(fruits);
+//  {
+//     let fruits;
+//     console.log(fruits);
+//     fruits="banana";
+//     console.log(fruits);
+
+//  }
+
+
+
+
+// console.log("hello world");
+
+// function hello(){
+//  let a=18;
+//  id=2;
+//  console.log(hello['a'])
+// }
+
+var c = 10;
+{
+var c = 20;
+console.log(c);
+}
+console.log(c);
---
+++ b.js
@@ -0,0 +1,18 @@
+// console.log(y)
+
+// war=10;
+// console.log(y)
+
+//    ex=1
+//      (function(){
+//     console.log("hello")
+// })()
+
+
+
+const hello= () =>{
+    console.log("hello world")
+
+}
+hello();
+     
